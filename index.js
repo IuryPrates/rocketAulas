164c317a9ddc1f74abca4c8cc6bc1edc1916ede4
IuryPrates <prates700@gmail.com>
2024-11-17 10:13:02 -0400

    edit http verbs

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+//Verbos HTTP
+// GET: Recebe dados do resource
+// POST: Enviar dados ou informações para serem processados por um Resource
+// PUT: Atualizar os dados de um Resource
+// DELETE: Deletar um Resource
+
 app.listen(3000, function(){
     console.log("Running in port 3000")
 });
