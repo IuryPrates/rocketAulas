417b57926d97da2ca61a5149db43bcfcae4e4f65
IuryPrates <prates700@gmail.com>
2024-11-17 11:11:54 -0400

    edit coment

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ app.get("/endpoint", function(req, res){ // cria o endpoint "./endpoint"
     res.json(data) // ao acessar ele irá rodar a funçção de callback que trará res. Contendo as informações de data (arquivo criado acima)
 })
 
+app.get("/endpoint/:id", function(req, res){
+    const { id } = req.params
+    const client = data.find(cli => cli.id == id)
+
+    // se não existir o cliente passado como parâmetro
+    if(!client)
+        return res.status(401).json();
+    
+    // retorno do cliente encontrado
+    res.json(client);
+});
+
 app.listen(3000, function(){ // inicia o servidor
     console.log("Running in port 3000")
+
+    //console.log(data)
 });
